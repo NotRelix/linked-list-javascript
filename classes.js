016d0bdd0e653b8ba5d0274c3528894dbbcecee0
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-04-19 15:49:34 +0800

    feat: add classes for linked list

---
+++ classes.js
@@ -0,0 +1,12 @@
+export class LinkedList {
+  constructor(list) {
+    this.list = list;
+  }
+}
+
+export class Node {
+  constructor(value) {
+    this.value = value;
+    this.nextNode = null;
+  }
+}
